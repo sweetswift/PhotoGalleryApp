bad141a0ff026ad3fd7fa5ff467ef874d755666c
LeeChungWoo <ios.lllccww@gmail.com>
2023-01-02 23:53:55 +0900

    [ADD] 앨범 접근 권한 코드 추가

--- PhotoGalleryApp/ViewController.swift
+++ PhotoGalleryApp/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 import PhotosUI
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var photoCollectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,15 +28,53 @@ class ViewController: UIViewController {
     }
     
     func makeNavigationItem() {
-        let photoItem = UIBarButtonItem(image: UIImage(systemName: "photo.on.rectangle"), style: .done, target: self, action: #selector(showGallery))
+        let photoItem = UIBarButtonItem(image: UIImage(systemName: "photo.on.rectangle"), style: .done, target: self, action: #selector(checkPermission))
         photoItem.tintColor = .black.withAlphaComponent(0.7)
         self.navigationItem.rightBarButtonItem = photoItem
         
-        let refreshItem = UIBarButtonItem(image: UIImage(systemName: "arrow.clockwise"), style: .done, target: self, action: #selector(showGallery))
+        let refreshItem = UIBarButtonItem(image: UIImage(systemName: "arrow.clockwise"), style: .done, target: self, action: #selector(checkPermission))
         self.navigationItem.leftBarButtonItem = refreshItem
     }
     
-    @objc func showGallery() {
+    @objc func checkPermission() {
+        if PHPhotoLibrary.authorizationStatus() == .authorized ||
+            PHPhotoLibrary.authorizationStatus() == .limited {
+            DispatchQueue.main.async {
+                self.showGallery()
+            }
+           
+            
+        }else if PHPhotoLibrary.authorizationStatus() == .denied {
+            DispatchQueue.main.async {
+                self.showAuthorizationDeniedAlert()
+            }
+            
+        }else if PHPhotoLibrary.authorizationStatus() == .notDetermined {
+            PHPhotoLibrary.requestAuthorization { status in
+                self.checkPermission()
+            }
+            
+        }
+        
+    }
+    
+    func showAuthorizationDeniedAlert() {
+        let alert = UIAlertController(title: "앨범 접근 권한을 활성화 해주세요", message: nil, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "닫기", style: .cancel))
+        alert.addAction(UIAlertAction(title: "설정으로 가기", style: .default, handler: { action in
+            
+            guard let url = URL(string: UIApplication.openSettingsURLString) else {
+                return
+            }
+            if UIApplication.shared.canOpenURL(url) {
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            }
+        }))
+        
+        self.present(alert, animated: true)
+    }
+    
+    func showGallery() {
         let libray = PHPhotoLibrary.shared()
         var configuration = PHPickerConfiguration(photoLibrary: libray)
         configuration.selectionLimit = 10
@@ -47,11 +85,11 @@ class ViewController: UIViewController {
         
         
     }
-
+    
     @objc func refresh() {
         
     }
-
+    
 }
 
 extension ViewController: UICollectionViewDataSource {
