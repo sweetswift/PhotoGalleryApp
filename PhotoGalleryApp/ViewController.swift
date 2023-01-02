60d446ab13a7233e2106296a6b60ebfd9e0c28ec
LeeChungWoo <ios.lllccww@gmail.com>
2023-01-02 22:41:33 +0900

    [ADD] NavigationItem 추가 (버튼,타이틀)

--- PhotoGalleryApp/ViewController.swift
+++ PhotoGalleryApp/ViewController.swift
@@ -11,9 +11,29 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.title = "Photo Gallery App"
+        makeNavigationItem()
+        
+ 
+    }
+    
+    func makeNavigationItem() {
+        let photoItem = UIBarButtonItem(image: UIImage(systemName: "photo.on.rectangle"), style: .done, target: self, action: #selector(showGallery))
+        photoItem.tintColor = .black.withAlphaComponent(0.7)
+        self.navigationItem.rightBarButtonItem = photoItem
+        
+        let refreshItem  = UIBarButtonItem(image: UIImage(systemName: "arrow.clockwise"), style: .done, target: self, action: #selector(showGallery))
+        self.navigationItem.leftBarButtonItem = refreshItem
+    }
+    
+    @objc func showGallery() {
+        
     }
 
+    @objc func refresh() {
+        
+    }
 
 }
 
